2feadbd94076959ec6fc41b58cc91c21e665317f
Miguel Botón <mboton@lowlevel-studios.com>
2020-10-16 15:54:40 +0200

    Updated rules. Updated version.

--- rules/style.js
+++ rules/style.js
@@ -14,6 +14,10 @@ module.exports = {
 
     'no-multi-spaces': 0,
 
+    'no-multiple-empty-lines': ['error', {
+      'max': 2
+    }],
+
     'no-whitespace-before-property': 0,
 
     'padded-blocks': 0,
